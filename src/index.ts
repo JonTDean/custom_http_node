49c213265b45f9f2abd793435f97c5cabf214559
Jonathan Dean <Jon@myishas-mbp.myfiosgateway.com>
2022-05-03 14:03:01 -0400

    Created a basic client request function

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,24 @@ const options = {
     // Specify HTTP Method
     method: "GET",
 };
+
+// Create a new request object that
+// targets the specfied options key values
+const req: ClientRequest = https.request(options, (res) => {
+    process.stdout.write(`statusCode: ${res.statusCode}`);
+    process.stdout.write(`headers: ${res.headers}`);
+
+    res.on("data", (d) => {
+        process.stdout.write(d);
+    });
+});
+
+// Handle errors and catch
+// them before they error out the process
+req.on("error", (e) => {
+    process.stderr.write(e as unknown as string);
+});
+
+// Close the request to free up resources
+// and avoid memory leaks
+req.end();
